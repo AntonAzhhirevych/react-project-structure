98c2f23b61645803b164633bbadbb7c56e586de8
Anton Azhhirevych <azhhirevych943@gmail.com>
2021-07-01 01:52:43 +0300

    test folder structure

---
+++ src/redux/middleware/enhancer.js
@@ -0,0 +1,10 @@
+// import { applyMiddleware } from 'redux';
+// import reduxThunk from 'redux-thunk';
+// import createSagaMiddleware from 'redux-saga';
+// import stateValidation from './state-validation';
+
+// const sagaMiddleware = createSagaMiddleware();
+
+// const enhancer = applyMiddleware(sagaMiddleware, reduxThunk, stateValidation);
+
+// export { enhancer, sagaMiddleware };
